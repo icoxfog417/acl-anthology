d0d2c9d8a354db17bf819d0bc18e7d72e32a6f35
icoxfog417 <icoxfog417@yahoo.co.jp>
2019-09-27 11:24:10 +0900

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="acl-anthology",
-    version="0.1.4",
+    version="0.1.5",
     description="Get ACL Anthology Papers.",
     keywords=["machine learning", "nlp", "natural language processing"],
     author="icoxfog417",
